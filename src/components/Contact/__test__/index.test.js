7865f334e52ec55ae8452d551c6ed857f37df33e
Ian Miller <imiller142@gmail.com>
2024-05-21 11:00:34 -0500

    added conditional rendering for contact form.

---
+++ src/components/Contact/__test__/index.test.js
@@ -0,0 +1,24 @@
+﻿import { render, cleanup, getByLabelText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Nav from '..';
+
+afterEach(cleanup);
+
+const categories = [
+    { name: 'portraits', description: 'Potraits of people in my life' }
+]
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
+it('renders', () => {
+    render(<Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockContactSelected}
+        />);
+})
